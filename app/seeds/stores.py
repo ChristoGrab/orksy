af73a6dd163e4ca52216a6e57765bf73a860782a
Christo Grabowski <christo.grab@gmail.com>
2022-11-22 11:56:06 -0600

    testing store seeds

--- app/seeds/stores.py
+++ app/seeds/stores.py
@@ -5,12 +5,17 @@ def seed_stores():
   snagrod = Store(
     name="Snagrod'z Sweet & Sour Squigz",
     description="Oi, you lot. We've got da best tasting squiz in da 'ole sector. Fresh as a newly-flailed humie. No refundz.",
-    owner_id=6,
-    banner_image=""
+    owner_id=6
+  )
+  mekkz = Store(
+    name="Da Iron Mekkworkz",
+    description="If yer lookin to git in a good roight tumble wif som puny humies, da best way ter get to 'em is wiv our shiny, fast mekkz.  Guaranteed ta take ya straight into the thikk of it, but not back out.",
+    owner_id=7
   )
 
-db.session.add(snagrod)
-db.session.commit()
+  db.session.add(snagrod)
+  db.session.add(mekkz)
+  db.session.commit()
 
 
 # Uses a raw SQL query to TRUNCATE or DELETE the users table. SQLAlchemy doesn't
