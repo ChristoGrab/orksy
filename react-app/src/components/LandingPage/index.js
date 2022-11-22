a997f537c0009e3c451e03841e1265784f917500
Christo Grabowski <christo.grab@gmail.com>
2022-11-22 17:41:58 -0600

    light styling to navbar

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -1,7 +1,14 @@
+import { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import './LandingPage.css'
+
 const LandingPage = () => {
   return (
-    <div>
-      'oy'
+    <div className="landing-page-container">
+      <div className="landing-page-grid">
+        
+      </div>
     </div>
   )
 }
