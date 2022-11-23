1c072d196b11324025d78e9fd2b5f5d6b809fb60
Christo Grabowski <christo.grab@gmail.com>
2022-11-22 22:57:14 -0600

    added backend route to create new Store with error validations

--- app/api/store_routes.py
+++ app/api/store_routes.py
@@ -1,7 +1,10 @@
-from flask import Blueprint
-from app.models import Store
+from flask import Blueprint, request
+from flask_login import login_required
+from app.models import Store, db
+from app.forms import StoreForm
 
 store_routes = Blueprint('stores', __name__)
+
 
 @store_routes.route('/')
 def all_stores():
@@ -10,11 +13,39 @@ def all_stores():
   """
   stores = Store.query.all()
   return {'stores': [store.to_dict() for store in stores]}
-  
+
+
 @store_routes.route('/<int:id>')
 def store(id):
     """
     Query for a store by id and returns that store in a dictionary
     """
     store = Store.query.get(id)
-    return store.to_dict()
+    if store:
+      return store.to_dict()
+    else:
+      return {"error": "This store does not exist"}
+
+
+@store_routes.route('/new', methods=["POST"])
+@login_required
+def create_store():
+  """
+  Create a new store
+  """
+  form = StoreForm()
+  form['csrf_token'].data = request.cookies['csrf_token']
+  
+  if form.validate_on_submit():
+    new_store = Store(
+      name = form.data["name"],
+      description = form.data["description"],
+      owner_id = form.data["owner_id"],
+      banner_image = form.data["banner_image"]
+    )
+    db.session.add(new_store)
+    db.session.commit()
+    
+    return new_store.to_dict()
+  else:
+    return form.errors
