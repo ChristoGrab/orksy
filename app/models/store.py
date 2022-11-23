241ad902ea860438ec76c8d5820489d3e1b72961
Christo Grabowski <christo.grab@gmail.com>
2022-11-23 12:49:12 -0600

    added conditional schema names to backend models to please angry render gods

--- app/models/store.py
+++ app/models/store.py
@@ -3,11 +3,14 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 class Store(db.Model):
   __tablename__ = "stores"
   
+  if environment == "production":
+    __table_args__ = {'schema': SCHEMA}
+  
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(100), nullable=False)
   description = db.Column(db.Text)
   banner_image = db.Column(db.String)
-  owner_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+  owner_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")))
 
   # Relationships
   owner = db.relationship("User", back_populates="store")
