562424131507cffc3cbbe47fea63b37445535c91
Christo Grabowski <christo.grab@gmail.com>
2022-11-27 16:07:03 -0600

    moved store owner links to store page

--- react-app/src/components/Profile/index.js
+++ react-app/src/components/Profile/index.js
@@ -6,7 +6,7 @@ import userAvatar from "../../assets/user-avatar.png"
 import "./Profile.css"
 
 const ProfilePage = () => {
-  
+
   const history = useHistory()
   const dispatch = useDispatch()
   const sessionUser = useSelector(state => state.session.user)
@@ -17,22 +17,20 @@ const ProfilePage = () => {
   useEffect(() => {
     dispatch(getMyStoreThunk())
   }, [dispatch])
-  
+
   if (!sessionUser) return null;
   let storeLinks;
-  
+
   if (store && !store.error) {
     storeLinks = (
       <div className="profile-store-functions">
-        {store.name}
-        <Link to={`/store/${store.id}/edit`}>Edit Yer Store</Link>
-        <Link to={`/store/${store.id}/delete`}>Delete Yer Store</Link>
+        <Link id="profile-page-store-link" to={`/store/${store.id}`}>{store.name}</Link>
       </div>
     )
   } else {
     storeLinks = (
       <div className="profile-store-functions">
-        <Link to="/store/create">Create Yer Store</Link>
+        <Link id="profile-page-store-link" to="/store/create">Create Yer Store</Link>
       </div>
     )
   }
@@ -40,13 +38,11 @@ const ProfilePage = () => {
   return (
     <div className="profile-page-container">
       <div className="profile-page-userinfo">
-      <img id="profile-page-avatar" src={userAvatar}></img>
-      <div id="profile-page-username">
-      {sessionUser.username}
-      </div>
-      </div>
-      <div className="profile-store-functions">
-        { storeLinks }
+        <img id="profile-page-avatar" src={userAvatar}></img>
+        <div id="profile-page-username">
+          {sessionUser.username}
+          {storeLinks}
+        </div>
       </div>
     </div>
   )
