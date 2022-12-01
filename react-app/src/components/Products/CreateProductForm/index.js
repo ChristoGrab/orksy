b51ce7452629da2a05ca8edc9a03d7c8b74cd1b5
Christo Grabowski <christo.grab@gmail.com>
2022-12-01 10:18:26 -0600

    added image validations to create product form

--- react-app/src/components/Products/CreateProductForm/index.js
+++ react-app/src/components/Products/CreateProductForm/index.js
@@ -26,13 +26,16 @@ const CreateProductForm = () => {
     dispatch(getMyStoreThunk())
   }, [dispatch])
 
+  let correctFile = true
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     const errorList = []
     if (!name.length) errorList.push("Yer gotta give da produkt a name")
-    if (name.length > 70) errorList.push("Dat namez too long 'n komplex for da boyz ta rememba'")
+    if (name.length > 50) errorList.push("Dat namez too long 'n komplex for da boyz ta rememba'")
     if (!description.length) errorList.push("Yer gotta give da produkt a derscripshun")
+    if (description.length > 1000) errorList.push("Da boyz don't like seein so many wordz...")
     if (price < 1 || price > 100000) errorList.push("Pick a price between 1 and 10000 teef")
 
     let myImage = document.querySelector("#imageInput")
@@ -47,8 +50,15 @@ const CreateProductForm = () => {
       }
 
       let img = myImage.files[0]
-      console.log(img)
 
+      if (img.type !== "image/gif" && img.type !== "image/jpeg" && img.type !== "image/png") {
+        correctFile = false
+      }
+
+      if (correctFile === false) errorList.push("Ye've gotta pick a valid filetype ta upload")
+      
+      if (errorList.length) return
+      
       formData.append('file', img)
 
       setSubmitted(true)
@@ -77,48 +87,48 @@ const CreateProductForm = () => {
     }
   }
 
-    return (
-      <div className="store-form-container">
-        <form className="store-form">
-          <div className="store-form-greeting">
-            Create Produkt
-          </div>
-          <div className="store-form-intro">
-            Lookin' ta earn sum extra teef? We'll get yer new shiny product on da market in no time, boss.
-          </div>
-          {errors.map((error, idx) =>
-            <div key={idx} className="error-message">{error}</div>)}
-          <label>Name</label>
-          <input className="store-form-input"
-            type="text"
-            required
-            value={name}
-            onChange={e => setName(e.target.value)} />
-          <label>Description</label>
-          <textarea className="store-form-textarea"
-            type="text"
-            required
-            value={description}
-            onChange={e => setDescription(e.target.value)} />
-          <label>Price - <span id="teef-note">Note: Orks use their teeth, or 'teef', as a form of currency</span></label>
-          <input className="store-form-input"
-            type="number"
-            value={price}
-            onChange={e => setPrice(e.target.value)} />
-          <label>Image</label>
-          <input type="file" name="file" id="imageInput" encType="multipart/form-data" />
-          <button className="store-form-button" onClick={handleSubmit}>Join the WAAAAAGH!!</button>
-        </form>
-        {submitted === true && (
-          <div className="loading-popup-container">
-            <div className="loading-popup-text">Yer request is in da workz, waitin' on da gretchins to finish da job</div>
-            <div className="loading-wheel-container"><i className="fa-solid fa-spinner"></i></div>
-          </div>
-        )}
-      </div>
-    )
-  }
+  return (
+    <div className="store-form-container">
+      <form className="store-form">
+        <div className="store-form-greeting">
+          Create Produkt
+        </div>
+        <div className="store-form-intro">
+          Lookin' ta earn sum extra teef? We'll get yer new shiny product on da market in no time, boss.
+        </div>
+        {errors.map((error, idx) =>
+          <div key={idx} className="error-message">{error}</div>)}
+        <label>Name - <span className="form-note">(max 50 chars)</span></label>
+        <input className="store-form-input"
+          type="text"
+          required
+          value={name}
+          onChange={e => setName(e.target.value)} />
+        <label>Description - <span className="form-note">(max 1000 chars)</span></label>
+        <textarea className="store-form-textarea"
+          type="text"
+          required
+          value={description}
+          onChange={e => setDescription(e.target.value)} />
+        <label>Price - <span className="form-note">(1-10000) Note: Orks use their teeth, or 'teef', as a form of currency</span></label>
+        <input className="store-form-input"
+          type="number"
+          value={price}
+          onChange={e => setPrice(e.target.value)} />
+        <label>Image - <span className='form-note'>Supported filetypes: png, jpg, gif</span></label>
+        <input type="file" name="file" id="imageInput" encType="multipart/form-data" />
+        <button className="store-form-button" onClick={handleSubmit}>Join the WAAAAAGH!!</button>
+      </form>
+      {submitted === true && (
+        <div className="loading-popup-container">
+          <div className="loading-popup-text">Yer request is in da workz, waitin' on da gretchins to finish da job</div>
+          <div className="loading-wheel-container"><i className="fa-solid fa-spinner"></i></div>
+        </div>
+      )}
+    </div>
+  )
+}
 
 
 
-  export default CreateProductForm;
+export default CreateProductForm;
