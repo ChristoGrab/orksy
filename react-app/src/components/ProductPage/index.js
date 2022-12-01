22dd6743ee25be6cac3ab31a299f9f4df4fab2e4
Christo Grabowski <christo.grab@gmail.com>
2022-11-30 18:13:20 -0600

    added shipping details to product page and updated styling

--- react-app/src/components/ProductPage/index.js
+++ react-app/src/components/ProductPage/index.js
@@ -10,22 +10,28 @@ const ProductPage = () => {
 
   const dispatch = useDispatch()
   const { productId } = useParams();
-  const [descrip, setDescrip] = useState(false)
-  const [shipping, setShipping] = useState(false)
+  const [descrip, setDescrip] = useState(true)
+  const [shipping, setShipping] = useState(true)
+  
+  // Calculate current date and set shipping date to week from today
+  let shippingDate = new Date()
+  shippingDate.setDate(new Date().getDate() + 7)
 
+  // get product and clear state
   useEffect(() => {
     dispatch(getProductThunk(productId))
-
     return (() => dispatch(clearProduct()))
   }, [dispatch, productId])
 
   if (!product) return null;
 
+  //function to toggle showing product description
   const showDescription = async (e) => {
     e.preventDefault();
     setDescrip(!descrip)
   }
 
+  //function to toggle showing shipping info
   const showShipping = async (e) => {
     e.preventDefault();
     setShipping(!shipping)
@@ -43,17 +49,25 @@ const ProductPage = () => {
           </Link>
           <div className="product-page-name">{product.name}</div>
           <div className="product-page-price"><i className="fa-solid fa-tooth" />{product.price}</div>
+          <div className="product-description-container">
+            <button id="show-description-button" onClick={showDescription}><span>Description</span><span><i className="fa-solid product-details-caret fa-caret-down" /></span></button>
+            {descrip === true && (
+              <div className="product-description-text">{product.description}</div>
+            )}
+            <button id="show-shipping-button" onClick={showShipping}><span>Shipping and return policies</span><span><i className="fa-solid product-details-caret fa-caret-down" /></span></button>
+            {shipping === true && (
+              <div className="product-shipping-info">
+                <div id="arrival-estimate-text">Estimated Arrival</div>
+                <div id="arrival-date-text">{shippingDate.toLocaleDateString('en-US')}</div>
+                <div id="shipping-cost-message">Cost to ship</div>
+                <div id="shipping-cost"><i className="fa-solid fa-tooth" />{product.price / 100}</div>
+                <div id="carbon-message">Orksy proudly kontributes to carbon emissions by using da finest pollutants in da sektor.</div>
+              </div>
+            )}
+          </div>
+          
+      
         </div>
-
-      </div>
-      <div className="product-description-container">
-        <button id="show-description-button" onClick={showDescription}><span>Description</span><span><i id="show-description-caret" className="fa-solid fa-caret-down" /></span></button>
-        {descrip === true && (
-          <div className="product-description-text">{product.description}</div>
-        )}
-      </div>
-      <div className="shipping-info">
-        <button id="show-shipping-button" onClick={showShipping}><span>Shipping and return policies</span><span><i className="fa-solid fa-caret-down" /></span></button>
       </div>
     </div>
   )
