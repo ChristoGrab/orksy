d57d5bd5ff82ec8444ff9d2753c2b43df085ec15
Christo Grabowski <christo.grab@gmail.com>
2022-12-01 17:54:18 -0600

    improved styling on product page

--- react-app/src/components/ProductPage/index.js
+++ react-app/src/components/ProductPage/index.js
@@ -12,10 +12,13 @@ const ProductPage = () => {
   const { productId } = useParams();
   const [descrip, setDescrip] = useState(true)
   const [shipping, setShipping] = useState(true)
-  
+
   // Calculate current date and set shipping date to week from today
+  let today = new Date()
   let shippingDate = new Date()
-  shippingDate.setDate(new Date().getDate() + 7)
+  shippingDate.setDate(new Date().getDate() + Math.floor(Math.random() * (6 - 3) + 3))
+  let deliveryDate = new Date()
+  deliveryDate.setDate(new Date().getDate() + Math.floor(Math.random() * (10 - 7) + 7))
 
   // get product and clear state
   useEffect(() => {
@@ -43,37 +46,72 @@ const ProductPage = () => {
         <div className="product-page-image-container">
           <img className="product-page-image" src={product.image} alt={product.name} />
         </div>
-        
+
         <div className="product-page-details-container">
           <Link to={`/store/${product.store_id}`}
             className="product-page-store-link">{product.store?.name}
           </Link>
           <div className="product-page-name">{product.name}</div>
           <div className="product-page-price"><i className="fa-solid fa-tooth" />{product.price}</div>
-          
+
           <div className="product-description-container">
-            <button id="show-description-button" onClick={showDescription}>
-              <span>Description</span>
-              <span><i className="fa-solid product-details-caret fa-caret-down" /></span>
-            </button>
-            {descrip === true && (
-              <div className="product-description-text">{product.description}</div>
-            )}
+            {descrip === true
+              ? <button id="show-description-button" onClick={showDescription}>
+                <span>Description</span>
+                <span><i className="fa-solid product-details-caret fa-caret-up" /></span>
+              </button>
+              : <button id="show-description-button" onClick={showDescription}>
+                <span>Description</span>
+                <span><i className="fa-solid product-details-caret fa-caret-down" /></span>
+              </button>
+            }
             
-            <button id="show-shipping-button" onClick={showShipping}>
-              <span>Shipping and return policies</span>
-              <span><i className="fa-solid product-details-caret fa-caret-down" /></span>
-            </button>
+            {descrip === true
+              ? <div className="product-description-text">{product.description}</div>
+              : <div className="product-description-text-closed"></div>
+            }
+
+            {shipping === true
+              ? <button id="show-shipping-button" onClick={showShipping}>
+                <span>Shipping and return policies</span>
+                <span><i className="fa-solid product-details-caret fa-caret-up" id="up"/></span>
+              </button>
+              : <button id="show-shipping-button" onClick={showShipping}>
+                <span>Shipping and return policies</span>
+                <span><i className="fa-solid product-details-caret fa-caret-down" id="down"/></span>
+              </button>
+            }
+            
             {shipping === true && (
               <div className="product-shipping-info">
-                <div id="arrival-estimate-text">Estimated Arrival</div>
-                <div id="arrival-date-text">{shippingDate.toLocaleDateString('en-US')}</div>
+                <div>
+                  <div id="arrival-estimate-text">Estimated Arrival</div>
+                  <div id="arrival-date-text">{deliveryDate.toLocaleDateString('en-US')}</div>
+                </div>
+                <div id="shipping-tracker-container">
+
+                  <div className="shipping-tracker" id="st-1">
+                    <i className="fa-regular fa-handshake ship-i"></i>
+                    {today.toLocaleDateString('en-US')}
+                    <span className="shipping-tracker-text">Order Placed</span>
+                  </div>
+                  <div className="shipping-tracker" id="st-2">
+                    <i className="fa-brands fa-space-awesome ship-i"></i>
+                    {shippingDate.toLocaleDateString('en-US')}
+                    <span className="shipping-tracker-text">Rokkit Leaves</span>
+                  </div>
+                  <div className="shipping-tracker" id="st-3">
+                    <i className="fa-solid fa-gift ship-i"></i>
+                    {deliveryDate.toLocaleDateString('en-US')}
+                    <span className="shipping-tracker-text">Delivered!</span>
+                  </div>
+                </div>
                 <div id="shipping-cost-message">Cost to ship</div>
                 <div id="shipping-cost"><i className="fa-solid fa-tooth" />{product.price / 100}</div>
                 <div id="carbon-message">Orksy proudly kontributes to carbon emissions by using da finest pollutants in da sektor.</div>
               </div>
             )}
-            
+
           </div>
         </div>
       </div>
