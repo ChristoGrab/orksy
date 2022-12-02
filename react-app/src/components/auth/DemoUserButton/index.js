e9610e0a9c746183a06b1f97b91b897a8367d856
Christo Grabowski <christo.grab@gmail.com>
2022-12-02 17:41:16 -0600

    fixed demo user button flashing error messages

--- react-app/src/components/auth/DemoUserButton/index.js
+++ react-app/src/components/auth/DemoUserButton/index.js
@@ -4,7 +4,13 @@ import { login } from '../../../store/session';
 const DemoUserButton = () => {
     const dispatch = useDispatch()
 
-    const demoLogin = () => dispatch(login('gork@teef.io', 'password2'))
+    const demoLogin = async (e) => {
+        e.preventDefault();
+        
+        
+        
+        dispatch(login('gork@teef.io', 'password2'))
+    }
 
     return (
         <button className='auth-submit-button' onClick={demoLogin}>
