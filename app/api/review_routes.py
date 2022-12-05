1b8d0bfaa74f3d6ce16532a0fbfb06e7a85a1a0a
Christo Grabowski <christo.grab@gmail.com>
2022-12-05 15:30:52 -0600

    setting up review routes

---
+++ app/api/review_routes.py
@@ -0,0 +1,16 @@
+from flask import Blueprint
+from flask_login import login_required, current_user
+from app.models import Review, db
+from app.forms import ReviewForm
+
+review_routes = Blueprint("reviews", __name__)
+
+# GET ALL REVIEWS FOR A PRODUCT
+@review_routes.route("/<int:id>")
+def product_reviews():
+  """
+  Query for all reviews from a specific product
+  """
+  reviews = Review.query.all()
+  return {"Reviews": [review.to_dict() for review in reviews]}
+
---
+++ app/seeds/reviews.py
@@ -0,0 +1,75 @@
+from app.models import db, Review, environment, SCHEMA
+
+def seed_reviews():
+  rev_1 = Review(
+    rating=4,
+    review="Liked it",
+    product_id=1,
+    reviewer_id=
+  )
+  rev_2 = Review(
+    rating=3,
+    review="Liked it",
+    product_id=2,
+    reviewer_id=
+  )
+  rev_3 = Review(
+    rating=4,
+    review="Liked it",
+    product_id=3,
+    reviewer_id=
+  )
+  rev_4 = Review(
+    rating=5,
+    review="Liked it",
+    product_id=4,
+    reviewer_id=
+  )
+  rev_5 = Review(
+    rating=2,
+    review="Liked it",
+    product_id=5,
+    reviewer_id=
+  )
+  rev_6 = Review(
+    rating=1,
+    review="Liked it",
+    product_id=1,
+    reviewer_id=
+  )
+  rev_7 = Review(
+    rating=5,
+    review="Liked it",
+    product_id=2,
+    reviewer_id=
+  )
+  rev_8 = Review(
+    rating=4,
+    review="Liked it",
+    product_id=3,
+    reviewer_id=
+  )
+  rev_9 = Review(
+    rating=4,
+    review="Liked it",
+    product_id=4
+    reviewer_id=
+  )
+  rev_10 = Review(
+    rating=3,
+    review="Liked it",
+    product_id=5,
+    reviewer_id=
+  )
+  rev_11 = Review(
+    rating=6,
+    review="Liked it",
+    product_id=1,
+    reviewer_id=
+  )
+  rev_12 = Review(
+    rating=2,
+    review="Liked it",
+    product_id=6,
+    reviewer_id=
+  )
