171e0a255958ec78a9dbaecb12d9a1e9fcade2d8
Christo Grabowski <christo.grab@gmail.com>
2022-12-30 06:35:37 -0600

    more review seed data

--- app/seeds/reviews.py
+++ app/seeds/reviews.py
@@ -119,7 +119,7 @@ def seed_reviews():
     rating=5,
     review="All hail da mighty gaze of da mighty mork, who is definitely more mighty than Gork.  Or was it da other way around?",
     product_id=25,
-    reviewer_id=10
+    reviewer_id=8
   )
   chain_toof_1 = Review(
     rating=1,
@@ -131,6 +131,54 @@ def seed_reviews():
     rating=4,
     review="Dakkajet's 'ave a lot of firepower, and dis model doesn't disappoint.  I was able ta shoot down multiple 'umie battlecraft at a time in dis.  Good times all around.",
     product_id=18,
+    reviewer_id=9
+  )
+  telly_blast_1 = Review(
+    rating=3,
+    review="Half da time dis weapon will obliterate your enemies insides wiv lots of shiny magic.  Da other half, it'll be you dat gets turned to mush. It's a dice roll.",
+    product_id=26,
+    reviewer_id=3
+  )
+  burna_1 = Review(
+    rating=4,
+    review="It'll turn most fingz ta toast, but 'arder enemies won't mind it so much.",
+    product_id=9,
+    reviewer_id=7
+  )
+  big_choppa_1 = Review(
+    rating=5,
+    review="I bought dis ta knock some 'eads in after a disagreement wiv da Bad Wolves clan, and suffice to say, our differences were settled to my liking. Works perfectly.",
+    product_id=19,
+    reviewer_id=6
+  )
+  deffgun_1 = Review(
+    rating=5,
+    review="Da deffgun sits nice 'n comfy on yer shoulder while you lob tons of dakka at da foe. I might be deaf, but I'm not sore.",
+    product_id=17,
+    reviewer_id=6
+  )
+  hammer_kroozer_1 = Review(
+    rating=5,
+    review="Used a classic ramming move ta slice a rogue trader in 'alf.  Laughed the whole way back to my fungus lair.",
+    product_id=11,
+    reviewer_id=6
+  )
+  klaw_1 = Review(
+    rating=5,
+    review="If yer lookin' ta cut somefin in 'alf, wether it's a Space Marine or a can of squig paste, dis is the tool for you.  Works perfectly as intended.",
+    product_id = 3,
+    reviewer_id=6
+  )
+  klaw_2 = Review(
+    rating=4,
+    review="Loved the sounds it made as it diced through an eldar battlemage. Loved the sounds he made as well.",
+    product_id=3,
+    reviewer_id=8
+  )
+  klaw_3 = Review(
+    rating=5,
+    review="Nuffin is more stylish then chargin' inta a good scrap wiv a big old klaw on the end of yer arm.",
+    product_id=3,
     reviewer_id=9
   )
 
@@ -156,6 +204,15 @@ def seed_reviews():
   db.session.add(mork_gaze_1)
   db.session.add(chain_toof_1)
   db.session.add(dakkajet_1)
+  db.session.add(telly_blast_1)
+  db.session.add(burna_1)
+  db.session.add(big_choppa_1)
+  db.session.add(deffgun_1)
+  db.session.add(hammer_kroozer_1)
+  db.session.add(klaw_1)
+  db.session.add(klaw_2)
+  db.session.add(klaw_3)
+  
   db.session.commit()
 
 def undo_reviews():
