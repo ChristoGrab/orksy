a6fcfd729b0659432a03e249f993a0bcc639178d
Christo Grabowski <christo.grab@gmail.com>
2023-01-02 12:25:16 -0600

    user reviews now properly rerender on edit/delete

--- react-app/src/components/Reviews/UserReviews/index.js
+++ react-app/src/components/Reviews/UserReviews/index.js
@@ -1,9 +1,6 @@
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect, useState } from "react";
 import { userReviewsThunk } from "../../../store/reviews";
-import { Modal } from '../../../context/Modal'
-import UpdateReviewModal from "../ProductReviews/UpdateReviewModal";
-import DeleteReviewModal from "../ProductReviews/DeleteReviewModal";
 import UserReviewCard from "./UserReviewCard"
 
 const UserReviews = () => {
@@ -11,28 +8,10 @@ const UserReviews = () => {
   const dispatch = useDispatch()
   const reviews = useSelector(state => Object.values(state.reviews.user))
 
-  const [updateReviewModal, setUpdateReviewModal] = useState(false)
-  const [deleteReviewModal, setDeleteReviewModal] = useState(false)
-
-  // onClick functions for modals
-  const showUpdateReviewForm = async (e) => {
-    e.preventDefault();
-
-    return setUpdateReviewModal(true)
-  }
-
-  const confirmDelete = async (e) => {
-    e.preventDefault();
-
-    return setDeleteReviewModal(true)
-  }
-
   useEffect(() => {
     dispatch(userReviewsThunk())
-  }, [dispatch]
+  }, [dispatch, reviews]
   )
-
-  console.log(reviews)
 
   return (
     <div className="user-reviews-container">
@@ -42,4 +21,5 @@ const UserReviews = () => {
     </div>
   )
 }
+
 export default UserReviews;
