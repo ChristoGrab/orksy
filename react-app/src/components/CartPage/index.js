4bbd62753309f4396566462166485fd9063c7bd2
Christo Grabowski <christo.grab@gmail.com>
2023-01-04 13:52:18 -0600

    cart page and cart icon use localStorage to populate data

--- react-app/src/components/CartPage/index.js
+++ react-app/src/components/CartPage/index.js
@@ -1,7 +1,16 @@
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import "./CartPage.css"
 
 const CartPage = () => {
+  
+  const [cartItems, setCartItems] = useState([])
+  
+  useEffect(() => {
+    setCartItems(JSON.parse(localStorage.getItem("cart")))
+    console.log(cartItems)
+  }, [])
+  
   return (
     <div className="cart-page-container">
       
@@ -10,8 +19,20 @@ const CartPage = () => {
         <strong>Orksy purchase protekshun:</strong> Shop konfidently on Orksy knowin' if somefin' goes wrong wiv an order, you kin always use da faulty produkt to whack some skulls in.
       </div>
       
-      <h2>Your kart is empty.</h2>
-      <Link id="cart-discover-link" to='/'>Discover somefin' unique ta fill it up</Link>
+      {cartItems?.length 
+        ? <div className="cart-items-box">
+        <h2>Itemz in yer kart:</h2>
+        <ul>{cartItems.map(item => (
+          <div key={item.id}>{item.description}</div>
+        ))}</ul>
+        </div>
+        : <div className="cart-items-box">
+            <h2>Your kart is empty.</h2>
+            <Link id="cart-discover-link" to='/'>Discover somefin' unique ta fill it up</Link>
+          </div>
+      }
+      
+      
     </div>
   )
 }
--- react-app/src/components/Navbar/ShoppingCart.js
+++ react-app/src/components/Navbar/ShoppingCart.js
@@ -1,9 +1,11 @@
+import { useEffect, useState } from "react"
 import { useHistory } from "react-router-dom";
 import "./ShoppingCart.css"
 
 const ShoppingCart = () => {
   
   const history = useHistory();
+  const [cartSize, setCartSize] = useState(0)
   
   const openCartPage = async (e) => {
     e.preventDefault();
@@ -11,8 +13,19 @@ const ShoppingCart = () => {
     history.push("/cart")
   }
   
+  useEffect(() => {
+    let itemsInCart = JSON.parse(localStorage.getItem('cart'))
+    
+    if (itemsInCart) setCartSize(itemsInCart.length)
+  }, [])
+
   return (
+      <div className="shopping-cart-box">
       <i className="fa-solid fa-cart-shopping" onClick={openCartPage}/>
+      {cartSize > 0 
+        ? <div id="shopping-cart-numbers">{cartSize}</div>
+        : <div id="shopping-cart-numbers no-display"></div>}
+      </div>
   )
 }
 
