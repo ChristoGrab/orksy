a14f0880c278c30ceea1bf78aed48b392f2559b0
Christo Grabowski <christo.grab@gmail.com>
2023-01-06 09:58:26 -0600

    commented out all code relating to unused tables

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,6 +4,6 @@ from .store import Store
 from .product import Product
 from .db import environment, SCHEMA
 from .review import Review
-from .order import Order
-from .order_item import OrderItem
-from .cart import Cart
+# from .order import Order
+# from .order_item import OrderItem
+# from .cart import Cart
--- app/models/cart.py
+++ app/models/cart.py
@@ -1,24 +1,24 @@
-from .db import db, environment, SCHEMA, add_prefix_for_prod
+# from .db import db, environment, SCHEMA, add_prefix_for_prod
 
-class Cart(db.Model):
-  __tablename__ = "carts"
+# class Cart(db.Model):
+#   __tablename__ = "carts"
   
-  if environment == "production":
-    __table_args__ = {"schema": SCHEMA}
+#   if environment == "production":
+#     __table_args__ = {"schema": SCHEMA}
     
-  id = db.Column(db.Integer, primary_key=True)
-  user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")))
-  order_id = db.Column(db.Integer, nullable=False)
+#   id = db.Column(db.Integer, primary_key=True)
+#   user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")))
+#   order_id = db.Column(db.Integer, nullable=False)
 
-  # Relationships
-  user = db.relationship("User", back_populates="cart")
-  product = db.relationship("Product", back_populates="cart")
+#   # Relationships
+#   user = db.relationship("User", back_populates="cart")
+#   product = db.relationship("Product", back_populates="cart")
   
-  def to_dict(self):
-    return {
-      "id": self.id,
-      "user_id": self.user_id,
-      "product_id": self.product_id,
-      "order_id": self.order_id,
-      "quantity": self.quantity
-    }
+#   def to_dict(self):
+#     return {
+#       "id": self.id,
+#       "user_id": self.user_id,
+#       "product_id": self.product_id,
+#       "order_id": self.order_id,
+#       "quantity": self.quantity
+#     }
