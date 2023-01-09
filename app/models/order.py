932641da71af5f168f08642993cd41612f8e55cd
Christo Grabowski <christo.grab@gmail.com>
2023-01-09 14:17:00 -0600

    commented migration and model to prevent render issues

--- app/models/order.py
+++ app/models/order.py
@@ -1,26 +1,26 @@
-from .db import db, environment, SCHEMA, add_prefix_for_prod
-import datetime
+# from .db import db, environment, SCHEMA, add_prefix_for_prod
+# import datetime
 
-class Order(db.Model):
-  __tablename__ = "orders"
+# class Order(db.Model):
+#   __tablename__ = "orders"
   
-  if environment == "production":
-    __table_args__ = {"schema": SCHEMA}
+#   if environment == "production":
+#     __table_args__ = {"schema": SCHEMA}
     
-  id = db.Column(db.Integer, primary_key=True)
-  user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")))
-  total = db.Column(db.Integer, nullable=False)
-  timestamp = db.Column(db.DateTime, default=datetime.datetime.now)
+#   id = db.Column(db.Integer, primary_key=True)
+#   user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")))
+#   total = db.Column(db.Integer, nullable=False)
+#   timestamp = db.Column(db.DateTime, default=datetime.datetime.now)
 
-  # Relationships
-  order_items = db.relationship("OrderItem", back_populates="order")
-  user = db.relationship("User", back_populates="orders")
+#   # Relationships
+#   order_items = db.relationship("OrderItem", back_populates="order")
+#   user = db.relationship("User", back_populates="orders")
   
-  def to_dict(self):
-    return {
-      "id": self.id,
-      "user_id": self.user_id,
-      "total": self.total,
-      "items": [item.to_dict() for item in self.order_items],
-      "timestamp": self.timestamp
-    }
+#   def to_dict(self):
+#     return {
+#       "id": self.id,
+#       "user_id": self.user_id,
+#       "total": self.total,
+#       "items": [item.to_dict() for item in self.order_items],
+#       "timestamp": self.timestamp
+#     }
