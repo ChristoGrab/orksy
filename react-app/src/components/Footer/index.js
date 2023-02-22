d89e1a9c2808ee0948c3485c8dfb93c062aae623
Christo Grabowski <christo.grab@gmail.com>
2023-02-22 17:02:05 -0600

    fixed meta tags for responsive design and added alt to footer links

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -5,10 +5,10 @@ const Footer = () => {
     <div className="main-page-footer">
       <div id="about-me">Made by Christo Grabowski. Orksy is a fullstack clone of Etsy inspired by the wild wackiness of the Orks in Warhammer 40K.</div>
       <div id="about-me-links">
-        <a href="https://github.com/ChristoGrab" target="_blank" rel="noreferrer">
+        <a href="https://github.com/ChristoGrab" aria-label="Link to developer's github profile" target="_blank" rel="noreferrer">
           <i className="fa-brands fa-github"></i>
         </a>
-        <a href="https://www.linkedin.com/in/christo-grabowski-894a82a6/" target="_blank" rel="noreferrer">
+        <a href="https://www.linkedin.com/in/christo-grabowski-894a82a6/" aria-label="Link to developer's Linkedin profile" target="_blank" rel="noreferrer">
           <i className="ourIco fa-brands fa-linkedin"></i>
         </a>
       </div>
